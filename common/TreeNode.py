c9096289afc2a3f988909abe8c0fa96190ed4ad7
xiaoyue <771592594@qq.com>
2022-02-03 08:25:55 +0800

    修改二叉树的层次遍历

--- common/TreeNode.py
+++ common/TreeNode.py
@@ -15,27 +15,25 @@ def traverse(node: TreeNode):
     if not node:
         print([])
     queue = [node]
-    path = []
     while queue:
-        node = queue.pop(0)
-        if node:
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-            path.append(node.val)
-        else:
-            queue.append(None)
-            queue.append(None)
-    print(path)
+        nodes = []
+        count = len(queue)
+        for i in range(count):
+            node = queue.pop(0)
+            nodes.append(node)
+            if node:
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            print(node, end=" ")
+        print()
 
 
 def create_tree(arr: List[int]):
     def rec_create(i):
-        if i >= len(arr):
+        if i >= len(arr) or arr[i] is None:
             return None
-        if arr[i] is None:
-            return arr[i]
         if i in node_dict:
             return node_dict[i]
         node = TreeNode(arr[i], rec_create(i * 2 + 1), rec_create(i * 2 + 2))
@@ -51,5 +49,5 @@ def create_tree(arr: List[int]):
 
 
 if __name__ == '__main__':
-    root = create_tree([1, 0, 1, 0, 1, 0, 1])
+    root = create_tree([1, 2, 3, 4, 5, 6, 7])
     traverse(root)
